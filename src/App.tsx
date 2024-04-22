8a8d387a073feb32299f0a3e44491a66e29594dc
bassaaaa <white.galaxys2.album2@gmail.com>
2024-04-22 22:07:09 +0900

    sidebarをハードコーディング

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.scss";
+import Chat from "./components/Chat/Chat";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      {/* sidebar */}
+      <Sidebar />
+
+      {/* chat */}
+      <Chat />
+    </div>
+  );
 }
 
 export default App;
