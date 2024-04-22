cf30844c639e353e5b5d64a71b560fd61436a29a
bassaaaa <white.galaxys2.album2@gmail.com>
2024-04-22 23:26:38 +0900

    Providerを追加

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
+import { Provider } from "react-redux";
+import { store } from "./app/store.ts";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
