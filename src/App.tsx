506465fd1ccec1fb3b65ec2c1b9c04f3b18c2553
bassaaaa <white.galaxys2.album2@gmail.com>
2024-04-23 22:26:52 +0900

    ログイン機能を実装

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,33 @@ import "./App.scss";
 import Chat from "./components/Chat/Chat";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Login from "./components/Login/Login";
-import { useAppSelector } from "./app/hooks";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { useEffect } from "react";
+import { auth } from "./firebase";
+import { login, logout } from "./features/userSlice";
 
 function App() {
   const user = useAppSelector((state) => state.user);
-  console.log(user);
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((loginUser) => {
+      console.log(loginUser);
+      if (loginUser) {
+        dispatch(
+          login({
+            uid: loginUser.uid,
+            photo: loginUser.photoURL,
+            email: loginUser.email,
+            displayName: loginUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
 
   return (
     <div className="App">
@@ -16,7 +38,9 @@ function App() {
           <Chat />
         </>
       ) : (
-        <Login />
+        <>
+          <Login />
+        </>
       )}
     </div>
   );
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,14 +1,22 @@
 import { Button } from "@mui/material";
 import "./Login.scss";
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../../firebase";
 
 const Login = () => {
+  const signIn = () => {
+    signInWithPopup(auth, provider).catch((err) => {
+      alert(err.message);
+    });
+  };
+
   return (
     <div className="login">
       <div className="loginLogo">
         <img src="./discordLogo.png" alt="" />
       </div>
 
-      <Button>ログイン</Button>
+      <Button onClick={signIn}>ログイン</Button>
     </div>
   );
 };
