6f87d50896c904e387793eccb9036611d83d58a3
bassaaaa <white.galaxys2.album2@gmail.com>
2024-04-24 23:31:41 +0900

    チャット欄にメッセージ内容を反映する処理を実装

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -6,9 +6,57 @@ import GifIcon from "@mui/icons-material/Gif";
 import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
 import ChatMessage from "./ChatMessage";
 import { useAppSelector } from "../../app/hooks";
+import { useEffect, useState } from "react";
+import { CollectionReference, DocumentData, DocumentReference, Timestamp, addDoc, collection, onSnapshot, serverTimestamp } from "firebase/firestore";
+import { db } from "../../firebase";
+
+interface Message {
+  timestamp: Timestamp;
+  message: string;
+  user: {
+    uid: string;
+    photo: string;
+    email: string;
+    displayName: string;
+  };
+}
 
 const Chat = () => {
+  const [inputText, setInputText] = useState<string>("");
+  const [messages, setMessages] = useState<Message[]>([]);
+
   const channelName = useAppSelector((state) => state.channel.channelName);
+  const channelId = useAppSelector((state) => state.channel.channelId);
+  const user = useAppSelector((state) => state.user.user);
+
+  useEffect(() => {
+    const collectionRef: CollectionReference<DocumentData> = collection(db, "channels", String(channelId), "messages");
+    onSnapshot(collectionRef, (snapshot) => {
+      const results: Message[] = [];
+      snapshot.docs.forEach((doc) => {
+        results.push({
+          timestamp: doc.data().timestamp,
+          message: doc.data().message,
+          user: doc.data().user,
+        });
+      });
+      setMessages(results);
+    });
+  }, [channelId]);
+
+  const sendMessage = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+
+    // channels collectionの中にあるmessages コレクション中にinputTextを追加する
+    const collectionRef: CollectionReference<DocumentData> = collection(db, "channels", String(channelId), "messages");
+
+    const docRef: DocumentReference<DocumentData> = await addDoc(collectionRef, {
+      message: inputText,
+      timestamp: serverTimestamp(),
+      user: user,
+    });
+    console.log(docRef);
+  };
 
   return (
     <div className="chat">
@@ -16,17 +64,16 @@ const Chat = () => {
       <ChatHeader channelName={channelName} />
       {/* chatMessage */}
       <div className="chatMessage">
-        <ChatMessage />
-        <ChatMessage />
-        <ChatMessage />
-        <ChatMessage />
+        {messages.map((message, index) => (
+          <ChatMessage key={index} timestamp={message.timestamp} message={message.message} user={message.user} />
+        ))}
       </div>
       {/* chatInput */}
       <div className="chatInput">
         <AddCircleOutlineIcon />
         <form>
-          <input type="text" placeholder="#Udemyへメッセージを送信" />
-          <button type="submit" className="chatInputButton">
+          <input type="text" placeholder="#Udemyへメッセージを送信" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInputText(e.target.value)} />
+          <button type="submit" className="chatInputButton" onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => sendMessage(e)}>
             送信
           </button>
         </form>
